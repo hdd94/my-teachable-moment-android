8af20d93ee9548798121b2d9eaa79bc31fe8a52e
duc <huyducdo@googlemail.com>
2018-02-17 15:41:19 +0100

    RecyclerView Prototyp funktioniert

---
+++ app/src/main/java/com/example/android/firebaseauthdemo/t_MoviesAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.firebaseauthdemo;
+
+import android.graphics.Movie;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by duc on 15.02.18.
+ */
+
+public class t_MoviesAdapter extends RecyclerView.Adapter <t_MoviesAdapter.MyViewHolder> {
+
+    private List<t_Movie> moviesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            genre = (TextView) view.findViewById(R.id.genre);
+            year = (TextView) view.findViewById(R.id.year);
+        }
+    }
+
+
+    public t_MoviesAdapter(List<t_Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.t_activity_movie_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        t_Movie movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+
+}
